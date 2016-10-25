129a9be93206181698b1227be93cea5f468bd71c
Vaishnavi Reddy <vaireddy11@gmail.com>
2016-10-24 20:09:46 -0700

    add searchHotel class

--- src/main/java/com/sqa/vr/adactin/SearchHotel.java
+++ src/main/java/com/sqa/vr/adactin/SearchHotel.java
@@ -10,6 +10,7 @@ package com.sqa.vr.adactin;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.*;
+import org.openqa.selenium.support.ui.*;
 
 /**
  * SearchHotel //ADDD (description of class)
@@ -29,9 +30,70 @@ public class SearchHotel {
 	/**
 	 * @param driver
 	 */
+	
+    @FindBy(id = "adult_room")
+    WebElement adultsPerRoom;
+ 
+    @FindBy(id = "datepick_in")
+    WebElement checkInDate;
+ 
+    @FindBy(id = "datepick_out")
+    WebElement checkOutDate;
+ 
+    @FindBy(id = "child_room")
+    WebElement childrenPerRoom;
+ 
+    @FindBy(id = "hotels")
+    WebElement hotels;
+ 
+    @FindBy(id = "location")
+    WebElement location;
+ 
+    @FindBy(id = "room_nos")
+    WebElement numOfRooms;
+ 
+    @FindBy(id = "Reset")
+    WebElement resetBtn;
+ 
+    @FindBy(id = "room_type")
+    WebElement roomType;
+ 
+    @FindBy(id = "Submit")
+    WebElement submitBtn;
+ 
+	
+	
+	
 	public SearchHotel(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
+	
+    public void search(String locationInput, String hotelsInput, String roomTypeInput, String numOfRoomsInput,
+    			String checkInDateInput, String checkOutDateInput, String adultsPerRoomInput, String childrenPerRoomInput) {
+	        Select locationSelect = new Select(this.location);
+	        locationSelect.selectByVisibleText(locationInput);
+	        
+	        Select hotelsSelect = new Select(this.hotels);
+	        hotelsSelect.selectByVisibleText(hotelsInput);
+	        
+	        Select roomTypeSelect = new Select(this.roomType);
+	        roomTypeSelect.selectByVisibleText(roomTypeInput);
+	        
+	        Select numOfRoomsSelect = new Select(this.numOfRooms);
+	        numOfRoomsSelect.selectByValue(numOfRoomsInput);
+	        
+	        this.checkInDate.clear();
+	        this.checkInDate.sendKeys(checkInDateInput);
+	        
+	        this.checkOutDate.clear();
+	        this.checkOutDate.sendKeys(checkOutDateInput);
+	        
+	        Select adultsPerRoomSelect = new Select(this.adultsPerRoom);
+	        adultsPerRoomSelect.selectByValue(adultsPerRoomInput);
+	        
+	        Select childrenPerRoomSelect = new Select(this.childrenPerRoom);
+	        childrenPerRoomSelect.selectByValue(childrenPerRoomInput);
+	    }
 	
 }
